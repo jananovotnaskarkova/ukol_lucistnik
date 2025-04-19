c0478afaf598053ce922f1f0ecc9761769a1a625
Jana <jana.skarkova@gmail.com>
2025-04-19 21:13:11 +0200

    ukol

---
+++ UkolLucistnik/Lucistnik.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace SimulatorLucistnika
+{
+    public class Lucistnik
+    {
+        private string Jmeno;
+        private int PocetSipu;
+
+        public Lucistnik(string jmeno, int pocetSipu)
+        {
+            Jmeno = jmeno;
+            PocetSipu = pocetSipu;
+        }
+
+        public void Vystrel()
+        {
+            if (PocetSipu > 0)
+            {
+                PocetSipu--;
+                Console.WriteLine("Lucistnik uspesne vystrelil");
+            }
+            else
+            {
+                Console.WriteLine("Lucistnik ma malo sipu, takze nemuze vystrelit");
+            }
+        }
+
+        public void PridejSipy(int pocet)
+        {
+            if (pocet > 0)
+            {
+                PocetSipu = PocetSipu + pocet;
+                Console.WriteLine($"Lucistnikovi bylo pridano: {pocet} sip{SklonovaniSipu(pocet)}");
+            }
+            else
+            {
+                Console.WriteLine("Nulovy ani zaporny pocet sipu nelze pridat");
+            }
+        }
+
+        public void ZobrazStav()
+        {
+            Console.WriteLine("=======Info o lucistnikovi=======");
+            Console.WriteLine($"Lucistnik se jmenuje {Jmeno} a ma {PocetSipu} sip{SklonovaniSipu(PocetSipu)}");
+        }
+
+        public static string SklonovaniSipu(int mnozstviSipu)
+        {
+            switch(mnozstviSipu)
+            {
+                case 2 or 3 or 4:
+                    return "y";
+                case >= 5 or 0:
+                    return "u";
+                default:
+                    return "";
+            }
+        }
+
+        public static void VypisMenu()
+        {
+            Console.WriteLine("==============Menu===============");
+            Console.WriteLine("1: vystreleni sipu");
+            Console.WriteLine("2: pridani sipu");
+            Console.WriteLine("3: ukonceni programu");
+            Console.WriteLine("=================================");
+        }
+    }
+}
---
+++ UkolLucistnik/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SimulatorLucistnika;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Lucistnik karel = new Lucistnik("Karel", 7);
+
+        while (true)
+        {
+            karel.ZobrazStav();
+            Lucistnik.VypisMenu();
+            string vyzva = "Zadej prosim cislo z menu";
+            int zadani = NactiCeleCisloZKonzole(vyzva);
+            
+            switch (zadani)
+            {
+                case 1:
+                    karel.Vystrel();
+                    break;
+                case 2:
+                    string vyzvaPocetSipu = "Zadej kolik sipu se ma pridat";
+                    int pocet = NactiCeleCisloZKonzole(vyzvaPocetSipu);
+                    karel.PridejSipy(pocet);
+                    break;
+                case 3:
+                    Console.WriteLine("Ukoncuji program");
+                    return;
+                default:
+                    Console.WriteLine("Neplatna volba, zadej prosim cislo z menu");
+                    break;
+            }
+        }
+    }
+
+    public static int NactiCeleCisloZKonzole(string vyzva)
+    {
+        Console.WriteLine(vyzva);
+        int cislo = 0;
+        bool validniVstup = false;
+        string chybovaHlaska = "Nezadal jsi kladne cele cislo, zkus to prosim znovu";
+
+        while (!validniVstup)
+        {
+            string vstup = Console.ReadLine();
+            if (int.TryParse(vstup, out cislo))
+            {
+                if (cislo > 0)
+                {
+                    validniVstup = true;
+                }
+                else
+                {
+                    Console.WriteLine(chybovaHlaska);
+                }
+            }
+            else
+            {
+                Console.WriteLine(chybovaHlaska);
+            }
+        }
+        return cislo;
+    }
+}
